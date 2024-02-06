244173d334e35d2a686c75e102c5f025ecf08105
agreen8 <111467563+agreen8@users.noreply.github.com>
2024-02-06 13:45:10 -0800

    Add files via upload

--- plot_breakdown_curves.py
+++ plot_breakdown_curves.py
@@ -10,18 +10,20 @@ import matplotlib.pyplot as plt
 import glob
 import os
 
-# trial_name_path = r'Stx_13_*_*.csv' #for stx 13+
+trial_name_path = r'Stx_13_*_*.csv' #for stx 13+
 # trial_name_path = r'Stx_12_*_*.csv' #for stx 12+
-trial_name_path = r'Stx_11_*_*.csv' #for stx 13+
-# trial_name_path = r'6545xt_Stx_13_*_*.csv' #for stx 13+
+# trial_name_path = r'Stx_11_*_*.csv' #for stx 11+
+# trial_name_path = r'6545xt_Stx_13_*_*.csv' #for 6545xt stx 13+
 for trialname in glob.glob(trial_name_path):
     print(trialname)
     data = np.loadtxt(trialname, delimiter=',')
     Vins = data.T[0]
     fracs = data.T[1]
     plt.plot(Vins,fracs, marker='o',markersize=12, linewidth=4)
+    plt.title('whatever you want', fontsize=34)
     plt.xlabel('collision voltage',fontsize=26)
     plt.ylabel('relative abundance',fontsize=26)
+    plt.minorticks_on()
     plt.xticks(fontsize=24)
     plt.yticks(fontsize=24)
 

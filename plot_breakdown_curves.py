a76c2aa73882cae3c5f2cd3ffd670b3327888b64
agreen8 <111467563+agreen8@users.noreply.github.com>
2024-02-06 13:43:33 -0800

    Add files via upload

---
+++ plot_breakdown_curves.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  6 13:33:54 2024
+
+@author: Austin
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+import os
+
+# trial_name_path = r'Stx_13_*_*.csv' #for stx 13+
+# trial_name_path = r'Stx_12_*_*.csv' #for stx 12+
+trial_name_path = r'Stx_11_*_*.csv' #for stx 13+
+# trial_name_path = r'6545xt_Stx_13_*_*.csv' #for stx 13+
+for trialname in glob.glob(trial_name_path):
+    print(trialname)
+    data = np.loadtxt(trialname, delimiter=',')
+    Vins = data.T[0]
+    fracs = data.T[1]
+    plt.plot(Vins,fracs, marker='o',markersize=12, linewidth=4)
+    plt.xlabel('collision voltage',fontsize=26)
+    plt.ylabel('relative abundance',fontsize=26)
+    plt.xticks(fontsize=24)
+    plt.yticks(fontsize=24)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
